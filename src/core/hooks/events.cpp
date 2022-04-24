989df3ef8499995f464ff4391b94e4cbed5d8cd5
k0ez13 <kauardgz@protonmail.com>
2022-04-24 15:21:06 -0300

    notifications fixed

--- src/core/hooks/events.cpp
+++ src/core/hooks/events.cpp
@@ -21,10 +21,10 @@ void Hooks::Events::EventListener::FireGameEvent(IGameEvent *event) {
 
             if (CONFIGBOOL("Misc>Misc>Hitmarkers>Hitlogs")) {
                 if (CONFIGBOOL("Legit>Backtrack>Backtrack") && Features::Backtrack::lastBacktrack > 4) {
-                    Features::Notifications::addNotification(ImColor(220, 220, 40), "[gs] backtracked %s %i ticks for %i health", info.name, Features::Backtrack::lastBacktrack, event->GetInt("dmg_health"));
+                    Features::Notifications::addNotification(ImColor(220, 220, 40), "[nt] backtracked %s %i ticks for %i health", info.name, Features::Backtrack::lastBacktrack, event->GetInt("dmg_health"));
                 }
                 else {
-                    Features::Notifications::addNotification(ImColor(220, 220, 40), "[gs] hit %s for %i health", info.name, event->GetInt("dmg_health"));
+                    Features::Notifications::addNotification(ImColor(220, 220, 40), "[nt] hit %s for %i health", info.name, event->GetInt("dmg_health"));
                 }
             }
 
@@ -60,7 +60,7 @@ void Hooks::Events::EventListener::FireGameEvent(IGameEvent *event) {
             Interfaces::engine->GetPlayerInfo(victim->index(), &info);
 
             if (CONFIGBOOL("Misc>Misc>Hitmarkers>Hitlogs")) {
-                Features::Notifications::addNotification(ImColor(220, 40, 40), "[gs] killed %s", info.name);
+                Features::Notifications::addNotification(ImColor(220, 40, 40), "[nt] killed %s", info.name);
             }
         }
     }
--- src/core/menu/tabs/devwindow.cpp
+++ src/core/menu/tabs/devwindow.cpp
@@ -24,7 +24,7 @@ void Menu::drawDevWindow() {
 
     ImGui::Checkbox("Demo window", &demoWindow);
     if (ImGui::Button("Send test notification")) {
-        Features::Notifications::addNotification(ImColor(30, 255, 30), "[gs] Test notification! %f", Interfaces::globals->realtime);
+        Features::Notifications::addNotification(ImColor(30, 255, 30), "[nt] Test notification! %f", Interfaces::globals->realtime);
     }
 
     ImGui::Text("Is in dangerzone: %s", strstr(Offsets::getLocalClient(-1)->m_szLevelNameShort, "dz_") ? "true" : "false");
